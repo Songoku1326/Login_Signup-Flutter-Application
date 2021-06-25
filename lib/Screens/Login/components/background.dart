91f3809a0cb90ad234fd0ae67b04094957ac2c0b
M B S SASANK <53135320+Songoku1326@users.noreply.github.com>
2021-06-25 20:47:27 +0530

    Update background.dart

--- lib/Screens/Login/components/background.dart
+++ lib/Screens/Login/components/background.dart
@@ -21,7 +21,7 @@ class Background extends StatelessWidget {
             bottom: 0,
             child: Image.asset(
               "assets/images/pexels-photo-40465.png",
-              height: size.height,
+              fit: BoxFit.fill,
             ),
           ),
           child,
